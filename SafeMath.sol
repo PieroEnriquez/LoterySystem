1a1ab7a766ea431ecf2dde1294d5095dc328ce0a
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-06-05 21:47:48 -0500

    Translating the library

--- SafeMath.sol
+++ SafeMath.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 
-// Implementacion de la libreria SafeMath para realizar las operaciones de manera segura
-// Fuente: "https://gist.github.com/giladHaimov/8e81dbde10c9aeff69a1d683ed6870be"
+// Implementation of SafeMath library to be able to safely make operations
+// From: "https://gist.github.com/giladHaimov/8e81dbde10c9aeff69a1d683ed6870be"
 
 library SafeMath{
-    // Restas
+    // Substraction
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
       assert(b <= a);
       return a - b;
     }
     
-    // Sumas
+    // Addition
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
       uint256 c = a + b;
       assert(c >= a);
       return c;
     }
     
-    // Multiplicacion
+    // Multiplication
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
